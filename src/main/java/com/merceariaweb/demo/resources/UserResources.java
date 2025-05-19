b062b994ba1960806f88bf59b9c0994c206663e3
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-05-19 10:00:56 -0300

    User entity & resource

---
+++ src/main/java/com/merceariaweb/demo/resources/UserResources.java
@@ -0,0 +1,22 @@
+package com.merceariaweb.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.merceariaweb.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Feanor", "feanor@email.com", "(55) 55555 - 5555", "123456" );
+		return ResponseEntity.ok().body(u);
+		
+		
+	}
+}
+ 
