7fdda692f65410a4adaccf357000d2b92559c65a
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-05-20 12:27:20 -0300

    Product criado

---
+++ src/main/java/com/merceariaweb/demo/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.merceariaweb.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.merceariaweb.demo.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
