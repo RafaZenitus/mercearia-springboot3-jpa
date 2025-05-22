9afd14859b329e54b195b3b43199a1ec5957a487
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-05-21 22:38:10 -0300

    Erro OrderItemRepository ajustado

--- src/main/java/com/merceariaweb/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/merceariaweb/demo/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.merceariaweb.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.merceariaweb.demo.entities.OrderItem;
+import com.merceariaweb.demo.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
--- src/main/java/com/merceariaweb/demo/services/UserService.java
+++ src/main/java/com/merceariaweb/demo/services/UserService.java
@@ -13,55 +13,55 @@ import com.merceariaweb.demo.repositories.UserRepository;
 import com.merceariaweb.demo.services.exception.DatabaseException;
 import com.merceariaweb.demo.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll()
-	{
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	public User findById(Long id)
-	{
+
+	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		//return obj.get();
+		// return obj.get();
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
-		return  repository.save(obj);
+		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}
-		catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}
-		catch (DataIntegrityViolationException e){
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	@Transactional
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
-		
+
 	}
 
 }
