29f12f2cf9a0c278826db6bfb9d3ac09415e19ff
Rafael  Viol <126819742+RafaZenitus@users.noreply.github.com>
2025-05-21 22:19:59 -0300

    Exception error para métodos delete e put

--- src/main/java/com/merceariaweb/demo/resources/exception/ResourseExceptionHandler.java
+++ src/main/java/com/merceariaweb/demo/resources/exception/ResourseExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.merceariaweb.demo.services.exception.DatabaseException;
 import com.merceariaweb.demo.services.exception.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourseExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databse(DatabaseException e, HttpServletRequest request){
+		String error = "databse error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/merceariaweb/demo/services/UserService.java
+++ src/main/java/com/merceariaweb/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.merceariaweb.demo.entities.User;
 import com.merceariaweb.demo.repositories.UserRepository;
+import com.merceariaweb.demo.services.exception.DatabaseException;
 import com.merceariaweb.demo.services.exception.ResourceNotFoundException;
 
 import jakarta.transaction.Transactional;
@@ -35,7 +38,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	@Transactional
